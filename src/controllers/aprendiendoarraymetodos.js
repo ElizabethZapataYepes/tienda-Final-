3adb042148ed508f6b225243f94e052161de753b
407 <407@academicos.cesde.edu.co>
2022-10-20 14:52:58 -0500

    se creo carpeta de tienda

---
+++ src/controllers/aprendiendoarraymetodos.js
@@ -0,0 +1,145 @@
+let productos=[
+
+    {
+        nombre:"CAMISETA ROSADA ILUSTRACIÓN",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Camisa1%2C2.jpg?alt=media&token=f0131f72-aac1-4a51-9028-79f2af8786e7","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/camisa1.jpg?alt=media&token=423449f2-e31c-494e-a139-e9d804f4044f"],
+        precio: 38000,
+        popularidad:4,
+        descripcion:"Hermosa camiseta rosada, con un lindo estampado, camiseta ancha, 100% de algodon" 
+    },
+    {
+        nombre:"PANTALÓN BÁSICO",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/pantalon1%2C2.jpg?alt=media&token=e44d2b4d-7dae-495c-98ef-1001407ec926","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/pantalon.jpg?alt=media&token=859d3c31-2622-4c72-9271-c1539ae60463"],
+        precio:102000,
+        popularidad:5,
+        descripcion:"Pantalón verde, ancho en talla S y talla M, tiene unos bolsillos en la parte de atrás y la parte del frente, es 100% algodón recomendado lavar a mano." 
+    },
+    {
+        nombre:"CHAQUETA BÁSICA",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/chaqueta.jpg?alt=media&token=76a48d51-287e-4dc1-8cb1-05a94d103b85","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/chaqueta1.jpg?alt=media&token=58533bea-577b-4b18-8acb-0ececc867391"],
+        precio:124000,
+        popularidad:3,
+        descripcion:"Nuestra linda chaqueta demin básica larga, en el hermoso color blanco. la tenemos disponible en los colores negro y blanco, cuenta con un resorte en la parte superior, y una capota nos sirve para cada situación"
+    },
+    {
+        nombre:"FALDA NEGRA",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Falda.jpg?alt=media&token=a3b7f11e-092a-4b1c-a22e-c41e91d0cb41","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/falda1.jpg?alt=media&token=acc6617c-8d54-460f-aaf2-3f08710cdbb9"],
+        precio:80000,
+        popularidad:5,
+        descripcion:"Falda negra de jeans, en la talla M,S y L, con una gran horma y corta cuenta con 75%algodón, 23.5%poliester y 1.5%elastano" 
+    },
+    {
+        nombre:"HAREMS CAFÉ BRILLO",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/hamers1.jpg?alt=media&token=aee54d2a-3f5a-4fa1-9972-8644fd57670a","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/hamers.jpg?alt=media&token=715c6e1c-69de-45e3-9bb3-5fdecc2ac02c"],
+        precio:74000,
+        popularidad:4,
+        descripcion:"Elegante harems, para cada ocasión, se puede usar para una ocasión especial o noche de fiesta, cuenta con un 89% de elestano" 
+    },
+    {
+        nombre:"ENTERIZO NARANJA",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Enterizo.jpg?alt=media&token=3c613f9a-f134-4f8f-a069-a08b2608f2d2","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Enterizo1.jpg?alt=media&token=25cf2193-2c3b-43d6-b5ef-1ec4b9ec26fa"],
+        precio:81690,
+        popularidad:3,
+        descripcion:"Si te gusta estar comoda esta es una muy buen opción, enterizo largo y ancho y básico" 
+    },
+    {
+        nombre:"ENTERIZO VERDE",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/enterizoshort.jpg?alt=media&token=a3bae43a-2f85-4635-b3ae-7c3c078adc39","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/enterizoshort1.jpg?alt=media&token=20e2a570-beba-45ed-9b6a-b96d83003519"],
+        precio:68000,
+        popularidad:4,
+        descripcion:"Enterizo verde miniprint, en forma de short corto y anchito para una excelente movilidad en 100% algodón y gran calidad." 
+    },
+    {
+        nombre:"JOGGERS CARGO CAFÉ",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Joggers.jpg?alt=media&token=1a222cc5-3f04-4776-b3e0-dad9769f3e0d","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Joggers1.jpg?alt=media&token=81245070-9af2-4326-a754-6fea2869794e"],
+        precio:86000,
+        popularidad:4,
+        descripcion:"Joggers café estrecho y comodo, 100% algodón." 
+    },
+    {
+        nombre:"JEANS BLANCO",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Jenas.jpg?alt=media&token=6ec586c9-7de7-4632-bf05-877b26e7136c","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Jenas1.jpg?alt=media&token=3dbef0e9-a241-4f1c-bf07-f9a40c8d1562"],
+        precio:106000,
+        popularidad:3,
+        descripcion:"Jeans wide leg tiro alto blanco ilustración, ancho con unos lindos dibujos que lo hacen resaltar" 
+    },
+    {
+        nombre:"CAMISA DE RAYAS",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/rayas1.jpg?alt=media&token=ab26a01d-b8aa-4099-8982-354dc3e8361a","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/rayas.jpg?alt=media&token=9ddb5270-9310-4fd6-9abd-055da1ebbcb2"],
+        precio:68000,
+        popularidad:4,
+        descripcion:"Camisa manga corta beige rayas, 100% algodón." 
+    },
+    {
+        nombre:"SHORTS",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Shorts.jpg?alt=media&token=55f8a207-30e7-44a9-92d9-7d37d6b4861b","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Shorts1.jpg?alt=media&token=d1d3845b-be3c-4642-91c2-efae18b9a048"],
+        precio:78100,
+        popularidad:4,
+        descripcion:"Shorts de jeans, azul corto con apliques, disponible en tallas S,M y L."
+    },
+    {
+        nombre:"BUZO DE DISNEY",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Buso.jpg?alt=media&token=43dd25c4-9c5a-41c8-ad36-5aa77b2fd838","https://firebasestorage.googleapis.com/v0/b/prjrestaurante-bf264.appspot.com/o/Buso1.jpg?alt=media&token=39c2baee-82f8-4a11-b280-c6c02a257b33"],
+        precio:100000,
+        popularidad:5,
+        descripcion:"Buzo animado de disney, blanco con un hermoso estampado, resorte en las mangas y capota " 
+    }
+
+]
+
+let fila=document.getElementById("fila")
+
+//RECORRIENDO EL ARREGLO
+productos.forEach(function(producto){
+    
+    //creando un div desde JS
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    //creando una tarjeta
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","shadow","text-center","h-100","p-5","bg-secondary")
+
+    //creando una IMAGEN
+    let imagen=document.createElement("img")
+    imagen.classList.add("img-fluid","w-100")
+    imagen.src=producto.fotos[0]
+
+    //DETECTANDO EVENTO DE MOUSE
+    imagen.addEventListener("mouseover",function(){
+        imagen.src=producto.fotos[1]
+    })
+    imagen.addEventListener("mouseleave",function(){
+        imagen.src=producto.fotos[0]
+    })
+
+    //crear el nombre
+    let nombre=document.createElement("h3")
+    nombre.classList.add("fw-bold")
+    nombre.textContent=producto.nombre
+
+    //crear el precio
+    let precio=document.createElement("h2")
+    precio.textContent="$"+producto.precio
+
+    //creando la descripcion
+    let descripcion=document.createElement("p")
+     descripcion.classList.add("text-danger","d-none")
+     descripcion.textContent=producto.descripcion
+
+    //crear la popularidad
+    let popularidad=document.createElement("h4")
+    popularidad.textContent=producto.popularidad
+    
+
+    //definiendo padres e hijos
+    tarjeta.appendChild(imagen)
+    tarjeta.appendChild(nombre)
+    tarjeta.appendChild(precio)
+    tarjeta.appendChild(descripcion)
+    tarjeta.appendChild(popularidad)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+
+
+})
